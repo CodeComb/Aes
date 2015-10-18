f257c69f504ee1eeec8fe8d5803952181d565f7e
あまみや ゆうこ <1@1234.sh>
2015-10-18 19:08:38 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Aes
+# Aes Crypto
+
+```
+using CodeComb.Security.Aes;
+
+...
+
+var aes = new AesCrypto();
+var x = aes.Encrypt("abc");
+var y = aes.Decrypt(x);
+Console.WriteLine("abc" == y ? "Yes" : "No");
+```
---
+++ src/CodeComb.Security.Aes/AesCryptoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeComb.Security.Aes;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AesCryptoExtensions
+    {
+        public static IServiceCollection AddAesCrypto(this IServiceCollection self, string PrivateKey = null, string IV = null)
+        {
+            return self.AddSingleton(x => new AesCrypto(PrivateKey, IV));
+        }
+    }
+}
